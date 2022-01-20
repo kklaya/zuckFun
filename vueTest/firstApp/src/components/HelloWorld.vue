3426eee78eeed748658a2488d800e7b98473f5fb
zuck <1105475412@qq.com>
2022-01-20 23:06:52 +0800

    日历初始化

--- vueTest/firstApp/src/components/HelloWorld.vue
+++ vueTest/firstApp/src/components/HelloWorld.vue
@@ -1,39 +1,28 @@
-<template>
-  <div class="hello">
-        <span>青春是一个短暂的美梦, 当你醒来时, 它早已消失无踪</span>
-    <el-divider></el-divider>
-    <el-calendar v-model="value">
-</el-calendar>
-  </div>
-
-</template>
-
 <script>
+
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
 export default {
-  name: 'HelloWorld',
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      value: new Date()
+      calendarPlugins: [ dayGridPlugin ]
     }
   }
 }
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<template>
+  <FullCalendar defaultView="dayGridMonth" :plugins="calendarPlugins" />
+</template>
+
+<style >
+
+@import '~@fullcalendar/core/main.css';
+@import '~@fullcalendar/daygrid/main.css';
+
 </style>
